18db47b298786ca3fb9d329850d5120edcc5aa14
Jessica Sánchez <michelle.sr16@gmail.com>
2019-04-04 07:48:17 -0600

    changed fetch to axios

--- src/App.js
+++ src/App.js
@@ -2,119 +2,104 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import TweetBox from './TweetBox';
 import Feed from './Feed';
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      tweets: [],
-      error: null,
-      isLoaded: true
-    }
+	constructor(props){
+		super(props);
+		this.state = {
+			tweets: [],
+			error: null,
+			isLoaded: true
+		}
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  componentDidMount() {
-    fetch("https://still-garden-88285.herokuapp.com/draft_tweets")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            tweets: result.draft_tweets
-          })
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error: error
-          })
-        }
-      )
-  }
+	componentDidMount() {
+		axios.get("https://still-garden-88285.herokuapp.com/draft_tweets")
+			.then((result) => {
+				this.setState({
+					isLoaded: true,
+					tweets: result.data.draft_tweets
+				})
+			})
+			.catch((error) => {
+				this.setState({
+						isLoaded: true,
+						error: error
+					})
+			})
+	}
 
-  handleSubmit(newText) {
-    this.setState({isLoaded: false});
+	handleSubmit(newText) {
+		this.setState({isLoaded: false});
 
-    // Static UI :(
-    // let newTweet = {
-    //   id: this.state.tweets.length + 1,
-    //   user_name: 'Yvone',
-    //   avatar: 'https://img.ifcdn.com/images/d3951bf44788590b80f69c0c65718f7a23eb33c645cb677ee335f81a6e785ee6_3.jpg',
-    //   created_at: '11-03-2019',
-    //   description: newText
-    // };
+		// Static UI :(
+		// let newTweet = {
+		//   id: this.state.tweets.length + 1,
+		//   user_name: 'Yvone',
+		//   avatar: 'https://img.ifcdn.com/images/d3951bf44788590b80f69c0c65718f7a23eb33c645cb677ee335f81a6e785ee6_3.jpg',
+		//   created_at: '11-03-2019',
+		//   description: newText
+		// };
 
-    // let tweets = this.state.tweets.slice();
+		// let tweets = this.state.tweets.slice();
 
-    // this.setState({ tweets: tweets.concat(newTweet) });
+		// this.setState({ tweets: tweets.concat(newTweet) });
 
-    // Dynamic UI !
-    let newTweet = {
-      user_name: 'Yvone',
-      avatar: 'https://img.ifcdn.com/images/d3951bf44788590b80f69c0c65718f7a23eb33c645cb677ee335f81a6e785ee6_3.jpg',
-      description: newText
-    };
+		// Dynamic UI !
+		let newTweet = {
+			user_name: 'Yvone',
+			avatar: 'https://img.ifcdn.com/images/d3951bf44788590b80f69c0c65718f7a23eb33c645cb677ee335f81a6e785ee6_3.jpg',
+			description: newText
+		};
 
-    let headers = {};
-    headers['Content-Type'] = 'application/json';
+		axios.post("https://still-garden-88285.herokuapp.com/draft_tweets", newTweet)
+			.then((result) => {
+				let newTweets = this.state.tweets.slice();
 
-    const options = {
-      headers: headers,
-      method: 'POST',
-      // credentials: 'include',
-      body: JSON.stringify(newTweet)
-    };
+					this.setState({
+						isLoaded: true,
+						tweets: newTweets.concat(result.data.draft_tweet)
+					});
+			})
+			.catch((error) => {
+				this.setState({
+						isLoaded: true,
+						error
+					});
+			})
+	}
 
-    fetch("https://still-garden-88285.herokuapp.com/draft_tweets", options)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          let newTweets = this.state.tweets.slice();
+	render() {
+		const { error, isLoaded, tweets } = this.state;
+		let content;
 
-          this.setState({
-            isLoaded: true,
-            tweets: newTweets.concat(result.draft_tweet)
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+		if (error) {
+			content = <div>Error: {error.message}</div>;
+		} else {
+			return (
+				content = (
+					<Fragment>
+						<TweetBox
+							onSubmitNewTweet={this.handleSubmit}
+						/>
+						<Feed 
+							tweets={tweets}
+							isLoaded={isLoaded}
+						/>
+					</Fragment>
+				)
+			);
+		}
 
-  render() {
-    const { error, isLoaded, tweets } = this.state;
-    let content;
-
-    if (error) {
-      content = <div>Error: {error.message}</div>;
-    } else {
-      return (
-        content = (
-          <Fragment>
-            <TweetBox
-              onSubmitNewTweet={this.handleSubmit}
-            />
-            <Feed 
-              tweets={tweets}
-              isLoaded={isLoaded}
-            />
-          </Fragment>
-        )
-      );
-    }
-
-    return (
-      <div className="App">
-        { content }
-      </div>
-    )
-  }
+		return (
+			<div className="App">
+				{ content }
+			</div>
+		)
+	}
 }
 
 export default App;
